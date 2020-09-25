9c8d63446b7a23c2aa43845a4f9d3d2479569de7
your name <xogml951@gmail.com>
2020-09-25 20:52:04 +0900

    2020/09/25

--- Socket_programing/socket programing 연습/socket programing 연습/socket_programing1.c
+++ Socket_programing/socket programing 연습/socket programing 연습/socket_programing1.c
@@ -3,7 +3,7 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/types.h>
-#include <netinet/in.h>
+#include <netinet/in.h>//unix
 #include <sys/socket.h>
 #include <sys/wait.h>
 #define PORT 3490
@@ -26,7 +26,7 @@ void main() {
 	struct sockaddr_in my_addr;
 	struct sockaddr_in their_addr;
 	int sin_size;
-
+	//socket()
 	if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
 		perror("socket");
 		exit(1)
@@ -36,12 +36,12 @@ void main() {
 	my_addr.sin_port = htons(MYPORT);
 
 	my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
+	//bind()
 	if (bind(sockfd, (struct sockaddr*)&my_addr, sizeof(struct sockaddr)) == -1) {
 		perror("bind");
 		exit(1);
 	}
-
+	//listen(),accept()
 	while (1) {
 		sin_size = sizeof(struct sockaddr_in);
 		if ((new_fd = accept(sockfd, (struct sockaddr*)&their_addr, &sin_size)) == -1) {
@@ -50,4 +50,19 @@ void main() {
 		}
 		printf("server: got connection from %s\n", inet_ntoa(their_addr.sin_addr));
 	}
+
+	//Client
+	if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
+		perror("socket");
+		exit(1);
+	}
+	their_addr.sin_family = AF_INET;
+	their_addr.sin_port = htons(Server_Portnumber);
+	their_addr.sin_addr = htonl(Server_IP_address);
+
+	if (connect(sockfd, (struct sockfaddr *)&their_addr, sizeof(struct sockaddr)) == -1) {
+		perror("connect");
+		exit(1);
+	}
+
 }
