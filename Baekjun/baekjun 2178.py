704f84360f77a93dd61c5d2c201c5ff4cda91412
your name <xogml951@gmail.com>
2020-10-01 22:06:12 +0900

    2020/10/01

--- Baekjun/baekjun 2178.py
+++ Baekjun/baekjun 2178.py
@@ -8,3 +8,12 @@ Created on Wed Sep 30 21:42:43 2020
 import sys
 
 inp=sys.stdin.readlines()
+maze=[]
+for i in range(len(inp)):
+    if i==0:
+        n,m=map(int,inp[i].split)
+    else:
+        maze.append(list(map(int,list(inp[i]))))
+        
+
+    
