3e092d74384de16f0bfb62c59abfafd2eb85432f
xogml123 <xogml951@gmail.com>
2020-10-24 18:12:59 +0900

    2020/10/24

---
+++ Baekjoon/baekjoon 11509.py
@@ -0,0 +1,27 @@
+import sys
+
+inp=sys.stdin.readlines()
+
+n=int(inp[0].strip('\n'))
+
+ballons=list(map(int,inp[1].split()))
+result=0
+
+while ballons:
+    j=0
+    m=max(ballons)
+
+    while len(ballons)-1 >= j:
+        if ballons[j]==m:
+            ballons[j]=-1
+            j+=1
+            m-=1
+        elif ballons[j]!=m:
+            j+=1
+            continue
+
+    result+=1
+
+print(result)
+
+
